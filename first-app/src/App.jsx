e299ea15933223d822fab14a327eb5d1b48450ea
useris <useris@gmail.com>
2024-09-13 11:22:11 +0300

    Creating react project

---
+++ first-app/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const App = () =>{
+  return(
+    <>
+      <h1>Sveiki visi</h1>
+      <div>Čia yra greitas pavyzdys</div>
+    </>
+  )
+}
+
+export default App
